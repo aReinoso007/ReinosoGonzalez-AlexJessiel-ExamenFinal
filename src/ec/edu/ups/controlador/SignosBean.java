abfd5a4862a4d4c19d32a5ba41a804a256cc28b2
aReinoso007 <ralexjessiel@gmail.com>
2020-07-28 11:42:04 -0500

    ejb signos bean

--- src/ec/edu/ups/controlador/SignosBean.java
+++ src/ec/edu/ups/controlador/SignosBean.java
@@ -12,6 +12,7 @@ import javax.inject.Named;
 import ec.edu.ups.ejb.CitaMedicaFacade;
 import ec.edu.ups.ejb.SignosVitalesFacade;
 import ec.edu.ups.entidad.CitaMedica;
+import ec.edu.ups.entidad.SignosVitales;
 
 @FacesConfig(version = FacesConfig.Version.JSF_2_3)
 @Named
@@ -47,11 +48,14 @@ public class SignosBean implements Serializable{
 		CitaMedica cita = new CitaMedica();
 		cita = ejbCitaMedicaFacade.find(codigoCita);
 		System.out.println("cita recuperada:"+cita);
-		double fc = Double.parseDouble(this.freCardiaca);
-		double fr = Double.parseDouble(this.freResp);
+		double frecuenciaCardiaca = Double.parseDouble(this.freCardiaca);
+		double frecuenciaRespiratoria = Double.parseDouble(this.freResp);
 		double presion  = Double.parseDouble(this.presion);
 		double saturacion = Double.parseDouble(this.saturacion);
 		double temperatura = Double.parseDouble(this.temperatura);
+		
+		ejbSignosVitalesFacade.create(new SignosVitales(presion, frecuenciaCardiaca, frecuenciaRespiratoria, temperatura, saturacion, cita));
+		System.out.println("registro con exito");
 		
 		return null;
 	}
@@ -88,43 +92,44 @@ public class SignosBean implements Serializable{
 		this.listaCita = listaCita;
 	}
 
-	public double getFreCardiaca() {
+
+	public String getFreCardiaca() {
 		return freCardiaca;
 	}
 
-	public void setFreCardiaca(double freCardiaca) {
+	public void setFreCardiaca(String freCardiaca) {
 		this.freCardiaca = freCardiaca;
 	}
 
-	public double getFreResp() {
+	public String getFreResp() {
 		return freResp;
 	}
 
-	public void setFreResp(double freResp) {
+	public void setFreResp(String freResp) {
 		this.freResp = freResp;
 	}
 
-	public double getPresion() {
+	public String getPresion() {
 		return presion;
 	}
 
-	public void setPresion(double presion) {
+	public void setPresion(String presion) {
 		this.presion = presion;
 	}
 
-	public double getSaturacion() {
+	public String getSaturacion() {
 		return saturacion;
 	}
 
-	public void setSaturacion(double saturacion) {
+	public void setSaturacion(String saturacion) {
 		this.saturacion = saturacion;
 	}
 
-	public double getTemperatura() {
+	public String getTemperatura() {
 		return temperatura;
 	}
 
-	public void setTemperatura(double temperatura) {
+	public void setTemperatura(String temperatura) {
 		this.temperatura = temperatura;
 	}
 
